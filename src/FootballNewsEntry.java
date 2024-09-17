890d674f962d5a488dd9ba5b837f6f37ea2a2f8b
Georgy Voskanyan <hello@voskanyan.dev>
2024-09-17 11:36:25 +0200

    FIX: fields and price calculation in FootballNewsEntry

--- src/FootballNewsEntry.java
+++ src/FootballNewsEntry.java
@@ -1,22 +1,29 @@
 public class FootballNewsEntry extends NewsEntry {
+    private int basePrice = 300;
 
-    public FootballNewsEntry(String title) {
+    public FootballNewsEntry(String title, String competition, String club, String player) {
         super(title);
-        this.basePrice = 300;
+        this.competition = competition;
+        this.club = club;
+        this.player = player;
     }
 
-    public int calculatePrice(boolean mentionsChampionsLeague, boolean mentionsBarcaOrMadrid, boolean mentionsFerranTorresOBenzema) {
+    protected String competition;
+    protected String club;
+    protected String player;
+
+    public int calculatePrice() {
         int finalPrice = basePrice;
-        if(mentionsChampionsLeague) {
+        if(competition.contains("Lliga de Campions")) {
             basePrice += 100;
         }
-        if(mentionsBarcaOrMadrid) {
+        if(club.contains("Barça") || club.contains("Madrid")) {
             basePrice += 100;
         }
-        if(mentionsFerranTorresOBenzema) {
+        if(player.contains("Ferran Torres") || player.contains("Benzema")) {
             basePrice += 50;
         }
-
+        return basePrice;
     }
 
 }
